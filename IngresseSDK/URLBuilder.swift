b24af1d25e3dcbd6ebaf6b7fa6a158dfd853139d
Rubens Gondek <rubens.gondek@gmail.com>
2017-01-11 10:39:22 -0200

    Add new files and make workspace to use Pods

---
+++ IngresseSDK/URLBuilder.swift
@@ -0,0 +1,42 @@
+//
+//  URLBuilder.swift
+//  IngresseSDK
+//
+//  Created by Rubens Gondek on 1/10/17.
+//  Copyright © 2017 Gondek. All rights reserved.
+//
+
+import Foundation
+
+class URLBuilder {
+    
+    var service: IngresseService!
+    
+    init (_ service: IngresseService) {
+        self.service = service
+    }
+    
+    func generateAuthString() -> String {
+        return "?publickey=\(service.publicKey)&signature=\(getSignature())&timestamp=\(getTimestamp())"
+    }
+    
+    func getTimestamp() -> String {
+        let df = DateFormatter()
+        df.dateFormat = "yyyy-MM-dd'T'HH:mm:ss'Z'"
+        df.timeZone = TimeZone(abbreviation: "GMT")
+        df.locale = Locale(identifier: "en_US_POSIX")
+        
+        return df.string(from: Date()).removingPercentEncoding!
+    }
+    
+    func getSignature() -> String {
+        let timestamp = getTimestamp()
+        
+        let data = service.publicKey.appending(timestamp)
+        let signature = data
+        
+        return signature.removingPercentEncoding!
+    }
+}
+
+
