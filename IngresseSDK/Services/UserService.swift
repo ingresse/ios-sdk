fab9ad84422960b528c003f620a26580576fdef7
Bissuh <bissuh@gmail.com>
2017-10-08 14:42:58 -0300

    Adding method to load user data giving ID.

--- IngresseSDK/Services/UserService.swift
+++ IngresseSDK/Services/UserService.swift
@@ -36,4 +36,53 @@ public class UserService: BaseService {
             delegate.didFailDownloadEvents(errorData: error)
         }
     }
+    
+    /**
+     Get user information
+
+     - parameter userId: User ID
+     - parameter completion: Callback block
+     */
+    public func getUser(userId:Int, userToken: String = "", completion: @escaping (_ success: Bool, _ response:[String:Any]?)->()) {
+
+        let path = "user/\(userId)"
+        let params = [
+            "fields" : "id,name,email,pictures",
+            "usertoken" : userToken
+        ]
+
+        let url = URLBuilder.makeURL(host: client.host, path: path, publicKey: client.publicKey, privateKey: client.privateKey, parameters: params)
+
+        client.restClient.GET(url: url) { (success: Bool, response: [String:Any]?) in
+            if !success {
+                completion(false, response)
+                return
+            }
+
+            let user = self.userApiToDict(response!)
+
+            completion(true, user)
+        }
+    }
+
+    private func userApiToDict(_ apiResponse: [String:Any]) -> [String:Any] {
+
+        var dict : [String:Any] = [:]
+
+        dict["name"]       = (apiResponse["name"] as? String) ?? ""
+        dict["email"]      = (apiResponse["email"] as? String) ?? ""
+
+        var picturesDict : [String:String] = [:]
+
+        if let pictures = apiResponse["pictures"] as? [String:String] {
+            picturesDict["small"] = pictures["small"]
+            picturesDict["medium"] = pictures["medium"]
+            picturesDict["large"] = pictures["large"]
+        }
+        dict["pictures"] = picturesDict
+
+        return dict
+
+    }
+
 }
