521bb125b5056bd52221258ac19dcf01cb18452b
Phillipi Unger Lino <phillipi.lino@ingresse.com>
2018-08-16 15:51:43 -0300

    Complete PaymentResponse tests

--- IngresseSDKTests/Model/PaymentResponseTests.swift
+++ IngresseSDKTests/Model/PaymentResponseTests.swift
@@ -29,4 +29,40 @@ class PaymentResponseTests: XCTestCase {
         XCTAssertEqual(obj?.total, 0.0)
         XCTAssertEqual(obj?.transactionId, "transactionId")
     }
+
+    func testFromJSON() {
+        var json = [String:Any]()
+        json["boleto"] = "boleto"
+        json["message"] = "message"
+        json["status"] = "status"
+        json["tax"] = 0.0
+        json["total"] = 0.0
+        json["transactionId"] = "transactionId"
+
+        // When
+        let obj = PaymentResponse.fromJSON(json)
+
+        // Then
+        XCTAssertNotNil(obj)
+        XCTAssertEqual(obj?.boleto, "boleto")
+        XCTAssertEqual(obj?.message, "message")
+        XCTAssertEqual(obj?.status, "status")
+        XCTAssertEqual(obj?.tax, 0.0)
+        XCTAssertEqual(obj?.total, 0.0)
+        XCTAssertEqual(obj?.transactionId, "transactionId")
+    }
+
+    func testInit() {
+        // When
+        let obj = PaymentResponse()
+
+        // Then
+        XCTAssertNotNil(obj)
+        XCTAssertEqual(obj.boleto, "")
+        XCTAssertEqual(obj.message, "")
+        XCTAssertEqual(obj.status, "")
+        XCTAssertEqual(obj.tax, 0)
+        XCTAssertEqual(obj.total, 0)
+        XCTAssertEqual(obj.transactionId, "")
+    }
 }
