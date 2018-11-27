1b637cff30461c7775fbb7b6298ce3f68eb6f82a
gondekr <rubens.gondek@gmail.com>
2018-11-27 12:10:27 -0200

    Add holder

--- IngresseSDK/Model/PaymentTicket.swift
+++ IngresseSDK/Model/PaymentTicket.swift
@@ -5,7 +5,12 @@
 public struct PaymentTicket: Encodable {
     public var guestTypeId: String = ""
     public var quantity: Int = 0
-    public var holder: String = ""
+    public var holder: [Holder] = []
+
+    public struct Holder: Encodable {
+        public var email: String = ""
+        public init() {}
+    }
 
     public init() {}
 }
