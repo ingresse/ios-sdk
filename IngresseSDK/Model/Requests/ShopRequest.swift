8811e6e387f4f96f7995e904f6f763c6c1fb1140
Phillipi Unger Lino <phillipi.lino@ingresse.com>
2018-11-28 17:06:46 -0200

    Create payment Service

--- IngresseSDK/Model/Requests/ShopRequest.swift
+++ IngresseSDK/Model/Requests/ShopRequest.swift
@@ -12,6 +12,17 @@ extension Request {
             public init() {}
         }
 
+        public struct CreditCard: Encodable {
+            public var number: String = ""
+            public var holderName: String = ""
+            public var expiracyMonth: Int = 0
+            public var expiracyYear: Int = 0
+            public var cvv: Int = 0
+            public var birthDate: String = ""
+
+            public init() {}
+        }
+
         public struct Free: Encodable {
             public var userId: String = ""
             public var eventId: String = ""
@@ -24,7 +35,16 @@ extension Request {
         }
 
         public struct Payment: Encodable {
+            public var userId: String = ""
+            public var transactionId: String = ""
+            public var creditcard: CreditCard?
+            public var installments: Int = 0
+            public var paymentMethod: String = ""
+            public var document: String = ""
+            public var postback: String = ""
+            public var ingeprefsPayload: String = ""
 
+            public init() {}
         }
     }
 }
--- IngresseSDK/Services/PaymentService.swift
+++ IngresseSDK/Services/PaymentService.swift
@@ -56,8 +56,31 @@ public class PaymentService: BaseService {
         client.restClient.POSTData(url: url, data: data, JSONData: true, onSuccess: { (response) in
             guard let newResponse = response["data"] as? [String: Any],
                 let paymentResponse = JSONDecoder().decodeDict(of: Response.Shop.Payment.self, from: newResponse) else {
-                onError(APIError.getDefaultError())
-                return
+                    onError(APIError.getDefaultError())
+                    return
+            }
+            onSuccess(paymentResponse)
+        }, onError: { (error) in
+            onError(error)
+        })
+    }
+
+    public func doPayment(request: Request.Shop.Payment, userToken: String, onSuccess: @escaping (_ response: Response.Shop.Payment) -> Void, onError: @escaping (_ error: APIError) -> Void) {
+        let url = URLBuilder(client: client)
+            .setPath("shop")
+            .addParameter(key: "usertoken", value: userToken)
+            .build()
+
+        guard let data = try? JSONEncoder().encode(request) else {
+            onError(APIError.getDefaultError())
+            return
+        }
+
+        client.restClient.POSTData(url: url, data: data, JSONData: true, onSuccess: { (response) in
+            guard let newResponse = response["data"] as? [String: Any],
+                let paymentResponse = JSONDecoder().decodeDict(of: Response.Shop.Payment.self, from: newResponse) else {
+                    onError(APIError.getDefaultError())
+                    return
             }
             onSuccess(paymentResponse)
         }, onError: { (error) in
