b070178aacbae4bf423170edd22ea96453d7d585
Phillipi Unger Lino <phillipi.lino@ingresse.com>
2019-01-30 14:59:14 -0200

    Pass encoded data to update user basic infos

---
+++ IngresseSDK/Model/Requests/UpdateUserRequests.swift
@@ -0,0 +1,34 @@
+//
+//  Copyright © 2019 Ingresse. All rights reserved.
+//
+
+extension Request {
+    public struct UpdateUser: Encodable {
+        public struct BasicInfos: Encodable {
+            public var userId: String = ""
+            public var userToken: String = ""
+            public var name: String?
+            public var lastname: String?
+            public var currentEmail: String?
+            public var newEmail: String?
+            public var phone: String?
+            public var cpf: String?
+            
+            public init() {}
+        }
+        
+        public struct BillingInfos: Encodable {
+            public var userId: String = ""
+            public var userToken: String = ""
+            public var zip: String = ""
+            public var street: String = ""
+            public var number: String = ""
+            public var complement: String = ""
+            public var district: String = ""
+            public var city: String = ""
+            public var state: String = ""
+            
+            public init() {}
+        }
+    }
+}
