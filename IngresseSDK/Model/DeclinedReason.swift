a31bcbcecf04e7901069a3585e9b9af02cdf37fd
Felipe Leme <lemefe21@gmail.com>
2019-12-27 16:23:26 -0300

    add declined reason in transaction data

--- IngresseSDK/Model/DeclinedReason.swift
+++ IngresseSDK/Model/DeclinedReason.swift
@@ -2,18 +2,24 @@
 //  Copyright © 2019 Ingresse. All rights reserved.
 //
 
-public class DeclinedReason: Codable {
-    public var message: String?
-    public var code: String?
+public class DeclinedReason: NSObject, Codable {
+    @objc public var message: String?
+    @objc public var declinedBy: String?
+    @objc public var code: String?
+    @objc public var createdAt: String?
 
     enum CodingKeys: String, CodingKey {
         case message
+        case declinedBy
+        case createdAt
         case code
     }
 
     public required init(from decoder: Decoder) throws {
         guard let container = try? decoder.container(keyedBy: CodingKeys.self) else { return }
         message = container.decodeKey(.message, ofType: String.self)
+        declinedBy = container.decodeKey(.declinedBy, ofType: String.self)
+        createdAt = container.decodeKey(.createdAt, ofType: String.self)
         code = container.decodeKey(.code, ofType: String.self)
     }
 }
