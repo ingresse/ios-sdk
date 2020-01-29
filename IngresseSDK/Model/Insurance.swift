61c4b34e58f817b75219fca6c723ee4fcdb77168
Tahbata Ali <tahbata@msn.com>
2020-01-29 18:26:21 -0300

    Add partner attr in insurance

--- IngresseSDK/Model/Insurance.swift
+++ IngresseSDK/Model/Insurance.swift
@@ -6,11 +6,13 @@ public class Insurance: Decodable {
     public var enabled: Bool = false
     public var rate: Float = 0
     public var value: Float = 0
+    public var partner: String = ""
     
     enum CodingKeys: String, CodingKey {
         case enabled
         case rate
         case value
+        case partner
     }
     
     public required init(from decoder: Decoder) throws {
@@ -18,5 +20,6 @@ public class Insurance: Decodable {
         enabled = container.decodeKey(.enabled, ofType: Bool.self)
         rate = container.decodeKey(.rate, ofType: Float.self)
         value = container.decodeKey(.value, ofType: Float.self)
+        partner = container.decodeKey(.partner, ofType: String.self)
     }
 }
