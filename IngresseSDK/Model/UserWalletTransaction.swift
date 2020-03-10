08562150dfc6c44688287d763897701ca586d9af
Fernando <fernando01ferreira@gmail.com>
2020-03-10 17:10:30 -0300

    receiving Int or String for EventId

--- IngresseSDK/Model/UserWalletTransaction.swift
+++ IngresseSDK/Model/UserWalletTransaction.swift
@@ -84,7 +84,7 @@ public struct UserWalletTransaction: Decodable {
     }
     
     public struct Event: Decodable {
-        public var id: String?
+        public var id: Any?
         public var title: String?
 
         enum CodingKeys: String, CodingKey {
@@ -95,7 +95,13 @@ public struct UserWalletTransaction: Decodable {
         public init(from decoder: Decoder) throws {
             guard let container = try? decoder.container(keyedBy: CodingKeys.self) else { return }
             title = try? container.decodeIfPresent(String.self, forKey: .title)
-            id = try? container.decodeIfPresent(String.self, forKey: .id)
+            do {
+
+                id = try container.decodeIfPresent(Int.self, forKey: .id)
+            } catch {
+
+                id = try? container.decodeIfPresent(String.self, forKey: .id)
+            }
         }
     }
     
