bc460c8eac47dc468e9d0b33139546c2c9b63a77
Felipe Leme <lemefe21@gmail.com>
2020-03-30 16:10:24 -0300

    create event image detail model and service

--- IngresseSDK/Model/Advertisement.swift
+++ IngresseSDK/Model/Advertisement.swift
@@ -6,15 +6,21 @@ public class Advertisement: NSObject, Codable {
     public var eventId: Int = -1
     public var cover: CoverAd?
     public var background: BackgroundAd?
+    public var imageLink: EventImageSizes?
+    public var imageDescription: EventImageDescription?
 
     enum CodingKeys: String, CodingKey {
         case cover
         case background
+        case imageLink = "start_image"
+        case imageDescription = "start_image_description"
     }
 
     public required init(from decoder: Decoder) throws {
         guard let container = try? decoder.container(keyedBy: CodingKeys.self) else { return }
         cover = try container.decodeIfPresent(CoverAd.self, forKey: .cover)
         background = try container.decodeIfPresent(BackgroundAd.self, forKey: .background)
+        imageLink = container.safeDecodeKey(.imageLink, to: EventImageSizes.self)
+        imageDescription = container.safeDecodeKey(.imageDescription, to: EventImageDescription.self)
     }
 }
--- IngresseSDK/Services/EventService.swift
+++ IngresseSDK/Services/EventService.swift
@@ -79,6 +79,25 @@ public class EventService: BaseService {
         }, onError: onError)
     }
 
+    /// Get event image detail
+    ///
+    /// - Parameters:
+    ///   - eventId: id of the event
+    public func getEventImageDetail(ofEvent eventId: Int, onSuccess: @escaping (_ ads: Advertisement) -> Void, onError: @escaping (_ errorData: APIError) -> Void) {
+
+        let url = URLBuilder(client: client)
+            .setPath("event/\(eventId)/attributes")
+            .addParameter(key: "filter", value: "advertisement")
+            .build()
+
+        client.restClient.GET(url: url, onSuccess: { (response) in
+            guard let eventImage = JSONDecoder().decodeDict(of: Advertisement.self, from: response) else { return }
+            onSuccess(eventImage)
+        }, onError: { (error) in
+            onError(error)
+        })
+    }
+
     /// Get advertisement info for event
     ///
     /// - Parameters:
