060ff4672c7dcbde3d1a3209caee2ad4aff9ca50
PhillipiLino <phillipi.lino@hotmail.com>
2020-05-15 11:41:14 -0300

    Fix reviews

--- IngresseSDK/Parser/URLBuilder.swift
+++ IngresseSDK/Parser/URLBuilder.swift
@@ -80,8 +80,7 @@ public class URLBuilder: NSObject {
         return self
     }
     
-    public func addParameter(key: String, value: Any?) -> URLBuilder {
-        guard let value = value else { return self }
+    public func addParameter(key: String, value: Any) -> URLBuilder {
         self.parameters[key] = "\(value)"
         
         return self
