d3a3f784579f6a2d229b5d72e278bf4768d50857
Fernando <fernando01ferreira@gmail.com>
2020-10-23 07:41:53 -0300

    Add cashless attribute on WalletEvent

---
+++ IngresseSDK/Model/WalletEventCashless.swift
@@ -0,0 +1,15 @@
+//
+//  Created by Fernando Ferreira on 22/10/20.
+//
+
+import Foundation
+
+public struct WalletEventCashless: Codable {
+
+    public let enabled: Bool
+
+    init() {
+
+        enabled = false
+    }
+}
