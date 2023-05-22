ed7fd76967475ed48cd166fa3a9390adf5e9b42b
PhillipiLino <phillipi.lino@gmail.com>
2023-05-22 14:36:02 -0300

    Remove unused file

--- IngresseSDK/Model/Requests/BackstageReportRequests.swift
+++
@@ -1,9 +0,0 @@
-//
-//  BackstageReportRequests.swift
-//  IngresseSDK
-//
-//  Created by Phillipi Unger Lino on 16/05/23.
-//  Copyright © 2023 Ingresse. All rights reserved.
-//
-
-import Foundation
