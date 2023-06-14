1dfab543a2fe2405d09c548237f17a6be100dd7d
PhillipiLino <phillipi.lino@gmail.com>
2023-06-14 18:00:20 -0300

    Change again login response device object

--- IngresseSDK/Model/UserDevice.swift
+++ IngresseSDK/Model/UserDevice.swift
@@ -7,30 +7,25 @@
 //
 
 public class UserDevice: Codable {
-    public let uuid: String?
     public let id: String?
     public let name: String?
-    public let createdAt: String?
-    public let active: Bool?
-    public let validated: Bool?
+    public let deviceType: String?
+    public let verified: Bool?
 
     enum CodingKeys: String, CodingKey {
-        case uuid
         case id
         case name
-        case createdAt = "created_at"
-        case active
-        case validated
+        case deviceType = "device_type"
+        case verified
     }
 
     public required init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        uuid = try? container.decodeIfPresent(String.self, forKey: .uuid) ?? ""
+
         id = try? container.decodeIfPresent(String.self, forKey: .id) ?? ""
         name = try? container.decodeIfPresent(String.self, forKey: .name) ?? ""
-        createdAt = try? container.decodeIfPresent(String.self, forKey: .createdAt) ?? ""
-        active = try? container.decodeIfPresent(Bool.self, forKey: .active) ?? false
-        validated = try? container.decodeIfPresent(Bool.self, forKey: .validated) ?? false
+        deviceType = try? container.decodeIfPresent(String.self, forKey: .deviceType) ?? ""
+        verified = try? container.decodeIfPresent(Bool.self, forKey: .verified) ?? false
     }
 }
 
